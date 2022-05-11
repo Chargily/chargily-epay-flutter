1a5b6730575da4a2afdb79bd8a60e3cc72e307cc
rainxh11 <rainxh11@gmail.com>
2022-05-11 02:48:07 +0100

    +version 1.0.0

--- chargily_flutter/lib/chargily.dart
+++ chargily_flutter/lib/chargily.dart
@@ -1,23 +1,28 @@
 library chargily_flutter;
 
-import 'package:chargily_flutter/invoice_validator.dart';
+import 'package:chargily_flutter/validations.dart';
 import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
 
 import 'chargily_api.dart';
 
 class Chargily {
-  String apiKey;
-  ChargilyApiClient? apiClient;
+  final String apiKey;
+  ChargilyApiClient? apiClient = ChargilyApiClient(Dio());
+  final Logger? logger;
 
-  Chargily(this.apiKey) {
-    apiClient = ChargilyApiClient(Dio());
-  }
+  /// Chargily Payment Gateway Client
+  ///
+  /// Get `apiKey` from https://epay.chargily.com.dz/secure/admin/epay-api
+  Chargily(this.apiKey, {this.logger, this.apiClient});
 
+  /// Create Payment from `invoice`
   Future<PaymentResponse> createPayment(Invoice invoice) async {
     final validator = InvoiceValidator();
 
     final validations = validator.validate(invoice);
     if (validations.hasError) {
+      logger?.e(validations.errors);
       return PaymentResponse(false, validations.errors);
     }
     final response = await apiClient?.createInvoice(apiKey, invoice);
