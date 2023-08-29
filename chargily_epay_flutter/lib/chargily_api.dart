14f29701ba92d54797366f94405b4df738ce5fba
Oussama MESSAOUDI <oussama.messaoudi.info@gmail.com>
2023-08-29 08:48:00 +0100

    - Upgraded dependencies on pubspec.yaml - added ignore_for_file: unused_element to chargily_api.g.dart - added ignore_for_file: constant_identifier_names to chargily_api.dart

--- chargily_epay_flutter/lib/chargily_api.dart
+++ chargily_epay_flutter/lib/chargily_api.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:dio/dio.dart' hide Headers;
 import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/retrofit.dart';
